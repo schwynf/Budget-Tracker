3eff5d4f0b2fb8029658f28dc16433bb40e63970
Schwyn Francis <schwynf@gmail.com>
2020-07-11 09:03:01 -0700

    organized directory and added indexedDB

---
+++ public/js/indexedDB.js
@@ -0,0 +1,50 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = function(event) {
+   // create object store called "pending" and set autoIncrement to true
+  const db = event.target.result;
+  db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+  console.log(navigator)
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+  //save data
+  db.transaction(["pending"], "readwrite").objectStore("pending").add(record);
+}
+
+function checkDatabase() {
+  const offlineData = db.transaction(["pending"], "readwrite").objectStore("pending").getAll();
+  console.log(offlineData);
+  offlineData.onsuccess = function() {
+    if (offlineData.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(offlineData.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => response.json())
+      .then(() => {
+        //clear pending data
+        db.transaction(["pending"], "readwrite").objectStore("pending").clear();
+      });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
